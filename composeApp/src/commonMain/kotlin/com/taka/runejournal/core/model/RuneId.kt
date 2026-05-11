09d1a4a97dbcb087dae74bba285da83cc403c393
DESKTOP-EGGQ356\Paolo <pmdalvarez@gmail.com>
2026-05-11 03:01:01 +0200

    all runes have borders now

--- composeApp/src/commonMain/kotlin/com/taka/runejournal/core/model/RuneId.kt
+++ composeApp/src/commonMain/kotlin/com/taka/runejournal/core/model/RuneId.kt
@@ -1,30 +1,36 @@
 package com.taka.runejournal.core.model
 
-enum class RuneId(val key: String) {
-    FEHU("fehu"),
-    URUZ("uruz"),
-    THURISAZ("thurisaz"),
-    ANSUZ("ansuz"),
-    RAIDHO("raidho"),
-    KENAZ("kenaz"),
-    GEBO("gebo"),
-    WUNJO("wunjo"),
-    HAGALAZ("hagalaz"),
-    NAUTHIZ("nauthiz"),
-    ISA("isa"),
-    JERA("jera"),
-    EIHWAZ("eihwaz"),
-    PERTHRO("perthro"),
-    ALGIZ("algiz"),
-    SOWILO("sowilo"),
-    TIWAZ("tiwaz"),
-    BERKANO("berkano"),
-    EHWAZ("ehwaz"),
-    MANNAZ("mannaz"),
-    LAGUZ("laguz"),
-    INGWAZ("ingwaz"),
-    DAGAZ("dagaz"),
-    OTHALA("othala");
+import org.jetbrains.compose.resources.DrawableResource
+import taka_rune_journal.composeapp.generated.resources.*
+
+enum class RuneId(
+    val key: String,
+    val drawable: DrawableResource,
+) {
+    FEHU("fehu", Res.drawable.rune_fehu),
+    URUZ("uruz", Res.drawable.rune_uruz),
+    THURISAZ("thurisaz", Res.drawable.rune_thurisaz),
+    ANSUZ("ansuz", Res.drawable.rune_ansuz),
+    RAIDHO("raidho", Res.drawable.rune_raidho),
+    KENAZ("kenaz", Res.drawable.rune_kenaz),
+    GEBO("gebo", Res.drawable.rune_gebo),
+    WUNJO("wunjo", Res.drawable.rune_wunjo),
+    HAGALAZ("hagalaz", Res.drawable.rune_hagalaz),
+    NAUTHIZ("nauthiz", Res.drawable.rune_nauthiz),
+    ISA("isa", Res.drawable.rune_isa),
+    JERA("jera", Res.drawable.rune_jera),
+    EIHWAZ("eihwaz", Res.drawable.rune_eihwaz),
+    PERTHRO("perthro", Res.drawable.rune_perthro),
+    ALGIZ("algiz", Res.drawable.rune_algiz),
+    SOWILO("sowilo", Res.drawable.rune_sowilo),
+    TIWAZ("tiwaz", Res.drawable.rune_tiwaz),
+    BERKANO("berkano", Res.drawable.rune_berkano),
+    EHWAZ("ehwaz", Res.drawable.rune_ehwaz),
+    MANNAZ("mannaz", Res.drawable.rune_mannaz),
+    LAGUZ("laguz", Res.drawable.rune_laguz),
+    INGWAZ("ingwaz", Res.drawable.rune_ingwaz),
+    DAGAZ("dagaz", Res.drawable.rune_dagaz),
+    OTHALA("othala", Res.drawable.rune_othala);
 
     companion object {
         fun fromKey(key: String): RuneId? =
--- composeApp/src/commonMain/kotlin/com/taka/runejournal/core/ui/AboutScreen.kt
+++ composeApp/src/commonMain/kotlin/com/taka/runejournal/core/ui/AboutScreen.kt
@@ -1,15 +1,26 @@
 package com.taka.runejournal.core.ui
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
+import org.jetbrains.compose.resources.painterResource
+import org.jetbrains.compose.ui.tooling.preview.Preview
+import taka_rune_journal.composeapp.generated.resources.Res
+import taka_rune_journal.composeapp.generated.resources.rune_ansuz
+import taka_rune_journal.composeapp.generated.resources.rune_fehu
+import taka_rune_journal.composeapp.generated.resources.rune_uruz
 
 @Composable
 fun AboutScreen(
@@ -26,11 +37,27 @@ fun AboutScreen(
             text = "About",
             style = MaterialTheme.typography.headlineMedium
         )
-
         Text(
             text = "Reusable starter for Kotlin Multiplatform Compose apps.",
             modifier = Modifier.padding(top = 8.dp),
             style = MaterialTheme.typography.bodyLarge
+        )
+    }
+}
+
+@Preview
+@Composable
+private fun RunePreview() {
+    Box(
+        modifier = Modifier
+            .size(120.dp)
+            .background(Color.White),
+        contentAlignment = Alignment.Center
+    ) {
+        Image(
+            painter = painterResource(Res.drawable.rune_ansuz),
+            contentDescription = null,
+            modifier = Modifier.size(80.dp)
         )
     }
 }
