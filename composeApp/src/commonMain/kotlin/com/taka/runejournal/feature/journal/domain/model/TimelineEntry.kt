680ee79ad80a8554d11ed41e9f47dabbe835aaf4
DESKTOP-EGGQ356\Paolo <pmdalvarez@gmail.com>
2026-05-23 19:37:50 +0200

    Cleaned up timeline sealed class

--- composeApp/src/commonMain/kotlin/com/taka/runejournal/feature/journal/domain/model/TimelineEntry.kt
+++ composeApp/src/commonMain/kotlin/com/taka/runejournal/feature/journal/domain/model/TimelineEntry.kt
@@ -6,28 +6,29 @@ import kotlin.time.Instant
 sealed class TimelineEntry {
   abstract val id: Int
   abstract val createdAt: Instant
+  abstract val notes: String?
 
   data class JournalEntry(
     override val id: Int,
     override val createdAt: Instant,
-    val text: String,
+    override val notes: String,
     val imageFileName: String?
   ) : TimelineEntry()
 
   data class SingleRuneReading(
     override val id: Int,
     override val createdAt: Instant,
+    override val notes: String?,
     val rune: DrawnRune,
-    val notes: String?
   ) : TimelineEntry()
 
   data class PpfRuneReading(
     override val id: Int,
     override val createdAt: Instant,
+    override val notes: String?,
     val pastRune: DrawnRune,
     val presentRune: DrawnRune,
     val futureRune: DrawnRune,
-    val notes: String?
   ) : TimelineEntry()
 
 
