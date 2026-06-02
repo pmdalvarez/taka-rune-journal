e42419c958c8854f2c4ef039955eab6ddb2d05a2
DESKTOP-EGGQ356\Paolo <pmdalvarez@gmail.com>
2026-06-02 23:01:03 +0200

    TakaTheme now in all screens

--- composeApp/src/commonMain/kotlin/com/taka/runejournal/App.kt
+++ composeApp/src/commonMain/kotlin/com/taka/runejournal/App.kt
@@ -2,10 +2,13 @@ package com.taka.runejournal
 
 import androidx.compose.runtime.*
 import com.taka.runejournal.core.navigation.AppNavHost
+import com.taka.runejournal.core.ui.theme.TakaTheme
 import org.jetbrains.compose.ui.tooling.preview.Preview
 
 @Composable
 @Preview
 fun App() {
-    AppNavHost()
+    TakaTheme {
+        AppNavHost()
+    }
 }
