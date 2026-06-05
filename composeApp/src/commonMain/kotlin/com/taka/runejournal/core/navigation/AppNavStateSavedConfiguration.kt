a72166085085fdace9b84020a4ad1378d5d47b2c
DESKTOP-EGGQ356\Paolo <pmdalvarez@gmail.com>
2026-06-06 00:19:51 +0200

    Fixing up nav 3

--- composeApp/src/commonMain/kotlin/com/taka/runejournal/core/navigation/AppNavStateSavedConfiguration.kt
+++ composeApp/src/commonMain/kotlin/com/taka/runejournal/core/navigation/AppNavStateSavedConfiguration.kt
@@ -3,13 +3,16 @@ package com.taka.runejournal.core.navigation
 import androidx.navigation3.runtime.NavKey
 import androidx.savedstate.serialization.SavedStateConfiguration
 import com.taka.runejournal.feature.reading.navigation.ReadingRoute
+import kotlinx.serialization.ExperimentalSerializationApi
 import kotlinx.serialization.modules.SerializersModule
 import kotlinx.serialization.modules.polymorphic
 
+@OptIn(ExperimentalSerializationApi::class)
 val appNavSavedStateConfiguration = SavedStateConfiguration {
   serializersModule = SerializersModule {
     polymorphic(NavKey::class) {
-      subclass(AppRoute::class, AppRoute.serializer())
-      subclass(ReadingRoute::class, ReadingRoute.serializer())    }
+      subclassesOfSealed<AppRoute>()
+      subclassesOfSealed<ReadingRoute>()
+    }
   }
 }
