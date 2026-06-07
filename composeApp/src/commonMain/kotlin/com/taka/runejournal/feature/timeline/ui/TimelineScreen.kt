01d8fca3bab6915e3a3bfab9f866344f08b39d56
DESKTOP-EGGQ356\Paolo <pmdalvarez@gmail.com>
2026-06-07 15:27:38 +0200

    LazyColumn put into timeline screen and deleted TimelineList component

--- composeApp/src/commonMain/kotlin/com/taka/runejournal/feature/timeline/ui/TimelineScreen.kt
+++ composeApp/src/commonMain/kotlin/com/taka/runejournal/feature/timeline/ui/TimelineScreen.kt
@@ -1,10 +1,10 @@
 package com.taka.runejournal.feature.timeline.ui
 
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.Button
 import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.MaterialTheme
@@ -16,7 +16,11 @@ import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
-import com.taka.runejournal.feature.timeline.ui.components.TimelineItemList
+import androidx.paging.compose.collectAsLazyPagingItems
+import androidx.paging.compose.itemKey
+import com.taka.runejournal.feature.timeline.ui.components.ActionButtons
+import com.taka.runejournal.feature.timeline.ui.components.GreetingSection
+import com.taka.runejournal.feature.timeline.ui.components.TimelineItemRow
 import org.jetbrains.compose.resources.stringArrayResource
 import taka_rune_journal.composeapp.generated.resources.Res
 import taka_rune_journal.composeapp.generated.resources.timeline_prompts
@@ -31,29 +35,34 @@ fun TimelineScreen(
     modifier: Modifier = Modifier
 ) {
     val uiState by viewModel.uiState.collectAsState()
+    val pagingItems = viewModel.timelineItems.collectAsLazyPagingItems()
     val prompts = stringArrayResource(Res.array.timeline_prompts)
     LaunchedEffect(prompts) {
         viewModel.setDailyPrompt(prompts)
     }
 
-    Column(
+    LazyColumn(
         modifier = modifier
             .fillMaxSize()
             .padding(24.dp),
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
-
-        TimelineItemList(
-            displayName = uiState.displayName,
-            prompt = uiState.prompt,
-            timelineItems = viewModel.timelineItems,
-            onTimelineDetailClick = onTimelineDetailClick,
-            onNewReadingClick = onNewReadingClick,
-            onNewJournalEntryClick = onNewJournalEntryClick,
-            testArea = { testArea(viewModel, onSettingsClick) }
-        )
-
+        item { GreetingSection(uiState.displayName, uiState.prompt) }
+        items(
+            count = pagingItems.itemCount,
+            key = pagingItems.itemKey { it.id }
+        ) { index ->
+            val item = pagingItems[index]
+            item?.let {
+                TimelineItemRow(
+                    it,
+                    onTimelineDetailClick
+                )
+            }
+        }
+        item { ActionButtons(onNewReadingClick, onNewJournalEntryClick) }
+        item { testArea(viewModel, onSettingsClick) }
     }
 }
 
