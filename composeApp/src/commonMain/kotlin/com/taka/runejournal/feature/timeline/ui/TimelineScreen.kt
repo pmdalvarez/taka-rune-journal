f16f837b74fc66bc29bcb04f94da3e3d6b242751
DESKTOP-EGGQ356\Paolo <pmdalvarez@gmail.com>
2026-06-08 19:46:51 +0200

    welcome screen logic almost done

--- composeApp/src/commonMain/kotlin/com/taka/runejournal/feature/timeline/ui/TimelineScreen.kt
+++ composeApp/src/commonMain/kotlin/com/taka/runejournal/feature/timeline/ui/TimelineScreen.kt
@@ -4,31 +4,21 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.text.KeyboardActions
-import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.Button
 import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.focus.onFocusChanged
-import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.unit.dp
 import androidx.paging.compose.collectAsLazyPagingItems
 import androidx.paging.compose.itemKey
 import com.taka.runejournal.feature.timeline.ui.components.ActionButtons
 import com.taka.runejournal.feature.timeline.ui.components.GreetingSection
 import com.taka.runejournal.feature.timeline.ui.components.TimelineItemRow
-import org.jetbrains.compose.resources.stringResource
-import taka_rune_journal.composeapp.generated.resources.Res
-import taka_rune_journal.composeapp.generated.resources.timeline_textfield_label_your_name
 
 @Composable
 fun TimelineScreen(
@@ -52,7 +42,8 @@ fun TimelineScreen(
             GreetingSection(
                 uiState.displayName,
                 uiState.dailyPrompt,
-                viewModel::initializeDailyPrompt
+                viewModel::initializeDailyPrompt,
+                viewModel::setDisplayName
             )
         }
         items(
@@ -72,43 +63,6 @@ fun TimelineScreen(
         item { testArea(viewModel, onSettingsClick) }
     }
 }
-
-//@Composable
-//fun DisplayNameTextField(
-//    displayName: String,
-//    onDisplayNameChanged: (String) -> Unit,
-//    modifier: Modifier = Modifier,
-//) {
-////    var nameInput by rememberSaveable(displayName) {
-////        mutableStateOf(displayName)
-////    }
-//
-//    fun saveName() {
-//        onDisplayNameChanged(nameInput.trim())
-//    }
-//
-//    OutlinedTextField(
-//        value = nameInput,
-//        onValueChange = { nameInput = it },
-//        label = {
-//            Text(stringResource(Res.string.timeline_textfield_label_your_name))
-//        },
-//        singleLine = true,
-//        keyboardOptions = KeyboardOptions(
-//            imeAction = ImeAction.Done,
-//        ),
-//        keyboardActions = KeyboardActions(
-//            onDone = {
-//                saveName()
-//            },
-//        ),
-//        modifier = modifier.onFocusChanged { focusState ->
-//            if (!focusState.isFocused) {
-//                saveName()
-//            }
-//        },
-//    )
-//}
 
 @Composable
 private fun testArea(viewModel: TimelineViewModel, onSettingsClick: () -> Unit) {
--- composeApp/src/commonMain/kotlin/com/taka/runejournal/feature/timeline/ui/components/GreetingSection.kt
+++ composeApp/src/commonMain/kotlin/com/taka/runejournal/feature/timeline/ui/components/GreetingSection.kt
@@ -1,17 +1,27 @@
 package com.taka.runejournal.feature.timeline.ui.components
 
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.setValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.onFocusChanged
+import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.unit.dp
 import org.jetbrains.compose.resources.stringArrayResource
 import org.jetbrains.compose.resources.stringResource
 import taka_rune_journal.composeapp.generated.resources.Res
 import taka_rune_journal.composeapp.generated.resources.timeline_greeting
 import taka_rune_journal.composeapp.generated.resources.timeline_prompts
+import taka_rune_journal.composeapp.generated.resources.timeline_textfield_label_your_name
 import taka_rune_journal.composeapp.generated.resources.timeline_welcome_greeting
 import taka_rune_journal.composeapp.generated.resources.timeline_welcome_prompt
 
@@ -20,8 +30,15 @@ fun GreetingSection(
   displayName: String?,
   dailyPrompt: String?,
   onInitializeDailyPrompt: (List<String>) -> Unit,
+  onDisplayNameEntered: (String) -> Unit
 ) {
-  if (displayName.isNullOrBlank()) {
+
+  // show Welcome screen there is no display name
+  // OR if display name but was only just saved (to stay in welcome screen)
+  var displayNameEntered by rememberSaveable { mutableStateOf(false) }
+  val showWelcomeScreen = displayName.isNullOrEmpty() || displayNameEntered
+
+  if (showWelcomeScreen) {
     // If no name given, show welcome greeting + prompt asking for name
     Text(
       text = stringResource(Res.string.timeline_welcome_greeting),
@@ -31,6 +48,16 @@ fun GreetingSection(
       text = stringResource(Res.string.timeline_welcome_prompt),
       modifier = Modifier.padding(top = 8.dp),
       style = MaterialTheme.typography.bodyLarge
+    )
+    DisplayNameTextField(
+      onSaveName = {
+        val displayName = it.trim()
+        if (displayName.isNotEmpty()) {
+          onDisplayNameEntered(displayName)
+          displayNameEntered = true
+        }
+      },
+      modifier = Modifier.padding(top = 24.dp)
     )
   } else {
     // If name is set, show greeting using their name with daily prompt
@@ -55,5 +82,34 @@ fun GreetingSection(
       )
     }
   }
+}
 
+@Composable
+fun DisplayNameTextField(
+    onSaveName: (String) -> Unit,
+    modifier: Modifier = Modifier,
+) {
+  var nameInput by rememberSaveable { mutableStateOf("") }
+
+  OutlinedTextField(
+      value = nameInput,
+      onValueChange = { nameInput = it },
+      label = {
+          Text(stringResource(Res.string.timeline_textfield_label_your_name))
+      },
+      singleLine = true,
+      keyboardOptions = KeyboardOptions(
+          imeAction = ImeAction.Done,
+      ),
+      keyboardActions = KeyboardActions(
+          onDone = {
+            onSaveName(nameInput)
+          },
+      ),
+      modifier = modifier.onFocusChanged() { focusState ->
+          if (!focusState.isFocused) {
+            onSaveName(nameInput)
+          }
+      }
+  )
 }
