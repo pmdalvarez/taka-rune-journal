602c969724f4b0b639bf84c5a6b6db72258d3a20
DESKTOP-EGGQ356\Paolo <pmdalvarez@gmail.com>
2026-06-25 00:28:33 +0200

    Common editor now used for editing and making new journal entry

--- composeApp/src/commonMain/kotlin/com/taka/runejournal/feature/timeline/ui/NewJournalEntryScreen.kt
+++ composeApp/src/commonMain/kotlin/com/taka/runejournal/feature/timeline/ui/NewJournalEntryScreen.kt
@@ -1,11 +1,6 @@
 package com.taka.runejournal.feature.timeline.ui
 
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.SnackbarHostState
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
@@ -15,20 +10,17 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.unit.dp
 import com.taka.runejournal.core.ui.UiEvent
 import com.taka.runejournal.core.ui.components.TakaScaffold
 import com.taka.runejournal.core.ui.components.TakaSnackbarHost
-import com.taka.runejournal.core.ui.components.TakaTextField
 import com.taka.runejournal.core.ui.components.TakaTopBar
 import com.taka.runejournal.core.ui.components.TakaTopBarAction
 import com.taka.runejournal.core.ui.components.TakaTopBarNavigationIcon
 import com.taka.runejournal.core.ui.components.showErrorSnackbar
+import com.taka.runejournal.feature.timeline.ui.components.JournalEntryEditor
 import org.jetbrains.compose.resources.getString
 import org.jetbrains.compose.resources.stringResource
 import taka_rune_journal.composeapp.generated.resources.Res
-import taka_rune_journal.composeapp.generated.resources.new_journal_entry_textfield_label_notes
-import taka_rune_journal.composeapp.generated.resources.new_journal_entry_textfield_label_title
 import taka_rune_journal.composeapp.generated.resources.new_journal_entry_title
 
 @Composable
@@ -70,22 +62,12 @@ fun NewJournalEntryScreen(
       )
     },
   ) { contentModifier ->
-    Column(
+    JournalEntryEditor(
       modifier = contentModifier,
-      verticalArrangement = Arrangement.spacedBy(24.dp),
-    ) {
-      TakaTextField(
-        value = titleInput,
-        onValueChange = { titleInput = it },
-        label = stringResource(Res.string.new_journal_entry_textfield_label_title),
-        singleLine = true,
-      )
-      TakaTextField(
-        value = notesInput,
-        onValueChange = { notesInput = it },
-        label = stringResource(Res.string.new_journal_entry_textfield_label_notes),
-        minLines = 5,
-      )
-    }
+      titleValue = titleInput,
+      titleOnValueChange = { titleInput = it },
+      notesValue = notesInput,
+      notesOnValueChange = { notesInput = it }
+    )
   }
 }
