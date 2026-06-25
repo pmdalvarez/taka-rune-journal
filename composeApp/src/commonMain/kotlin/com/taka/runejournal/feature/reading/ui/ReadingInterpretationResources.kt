d3d51bdf4d8526185e10b3ec2ae2193fe30b2cee
DESKTOP-EGGQ356\Paolo <pmdalvarez@gmail.com>
2026-06-25 20:56:03 +0200

    Added new method to get interpretations of readings

---
+++ composeApp/src/commonMain/kotlin/com/taka/runejournal/feature/reading/ui/ReadingInterpretationResources.kt
@@ -0,0 +1,29 @@
+package com.taka.runejournal.feature.reading.ui
+
+import com.taka.runejournal.core.domain.model.DrawnRune
+import com.taka.runejournal.core.domain.model.ReadingCategory
+import org.jetbrains.compose.resources.StringResource
+import taka_rune_journal.composeapp.generated.resources.Res
+import taka_rune_journal.composeapp.generated.resources.allStringResources
+
+fun getReadingInterpretationRes(
+  drawnRune: DrawnRune,
+  readingCategory: ReadingCategory,
+): StringResource? {
+  val specificKey = getReadingInterpretationKey(
+    drawnRune = drawnRune,
+    readingCategory = readingCategory,
+  )
+
+  val generalKey = getReadingInterpretationKey(
+    drawnRune = drawnRune,
+    readingCategory = ReadingCategory.GENERAL,
+  )
+
+  return Res.allStringResources[specificKey]
+    ?: Res.allStringResources[generalKey]
+}
+
+private fun getReadingInterpretationKey(drawnRune: DrawnRune, readingCategory: ReadingCategory): String {
+  return "interpretation_${drawnRune.id.key}_${drawnRune.orientation.key}_${readingCategory.key}"
+}
