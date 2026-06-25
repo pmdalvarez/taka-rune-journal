d6deebc9904a18ea1e564627ce1470e1371ab3ce
DESKTOP-EGGQ356\Paolo <pmdalvarez@gmail.com>
2026-06-25 18:44:41 +0200

    Made journal entry detail screen nicer looking

--- composeApp/src/commonMain/kotlin/com/taka/runejournal/feature/timeline/ui/components/JournalEntryDetail.kt
+++ composeApp/src/commonMain/kotlin/com/taka/runejournal/feature/timeline/ui/components/JournalEntryDetail.kt
@@ -1,9 +1,12 @@
 package com.taka.runejournal.feature.timeline.ui.components
 
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
@@ -30,11 +33,24 @@ fun JournalEntryDetail(
       style = MaterialTheme.typography.labelMedium,
       color = MaterialTheme.colorScheme.onSurfaceVariant
     )
-    Text(
-      text = notes,
-      modifier = Modifier.padding(top = 24.dp),
-      style = MaterialTheme.typography.bodyLarge,
-      color = MaterialTheme.colorScheme.onBackground
-    )
+    Surface(
+      shape = MaterialTheme.shapes.small,
+      color = MaterialTheme.colorScheme.surfaceContainer,
+      contentColor = MaterialTheme.colorScheme.onSurface,
+      border = BorderStroke(
+        width = 1.dp,
+        color = MaterialTheme.colorScheme.outlineVariant,
+      ),
+      modifier = Modifier
+        .fillMaxWidth()
+        .padding(top = 24.dp)
+    ) {
+      Text(
+        text = notes,
+        modifier = Modifier.padding(16.dp),
+        style = MaterialTheme.typography.bodyLarge,
+        color = MaterialTheme.colorScheme.onSurfaceVariant,
+      )
+    }
   }
 }
