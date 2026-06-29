803ca403f6823a9a60db4158f69f53208bdfe8f6
DESKTOP-EGGQ356\Paolo <pmdalvarez@gmail.com>
2026-06-30 01:52:24 +0200

    Rune added to tab

--- composeApp/src/commonMain/kotlin/com/taka/runejournal/feature/reading/ui/components/ReadingInterpretationNotesTab.kt
+++ composeApp/src/commonMain/kotlin/com/taka/runejournal/feature/reading/ui/components/ReadingInterpretationNotesTab.kt
@@ -1,18 +1,23 @@
 package com.taka.runejournal.feature.reading.ui.components
 
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import com.taka.runejournal.core.domain.model.DrawnRune
+import androidx.compose.ui.Modifier
 import com.taka.runejournal.core.ui.components.TakaCard
-import org.jetbrains.compose.resources.StringResource
 
 @Composable
 fun ReadingInterpretationNotesTab(notes: String?) {
-  TakaCard {
+  TakaCard(
+    modifier = Modifier
+      .fillMaxHeight()
+  ) {
     Text(
+      modifier = Modifier.fillMaxWidth(),
       text = notes ?: "No notes yet",
-      style = MaterialTheme.typography.headlineMedium
+      style = MaterialTheme.typography.bodyMedium
     )
   }
 }
--- composeApp/src/commonMain/kotlin/com/taka/runejournal/feature/reading/ui/components/ReadingInterpretationRuneTab.kt
+++ composeApp/src/commonMain/kotlin/com/taka/runejournal/feature/reading/ui/components/ReadingInterpretationRuneTab.kt
@@ -1,19 +1,30 @@
 package com.taka.runejournal.feature.reading.ui.components
 
-import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.rotate
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
 import com.taka.runejournal.core.domain.model.DrawnRune
+import com.taka.runejournal.core.domain.model.RuneOrientation
 import com.taka.runejournal.core.ui.components.TakaCard
+import com.taka.runejournal.core.ui.drawable
+import com.taka.runejournal.core.ui.theme.TakaContentSpacing
 import com.taka.runejournal.feature.reading.ui.toDotSeparatedKeywords
 import org.jetbrains.compose.resources.StringResource
+import org.jetbrains.compose.resources.painterResource
 import org.jetbrains.compose.resources.stringResource
+import taka_rune_journal.composeapp.generated.resources.Res
+import taka_rune_journal.composeapp.generated.resources.rune_display_name_reversed
 
 @Composable
 fun ReadingInterpretationRuneTab(
@@ -27,14 +38,30 @@ fun ReadingInterpretationRuneTab(
     modifier = Modifier
       .fillMaxHeight()
   ) {
+    val drawnRuneName = if (drawnRune.orientation == RuneOrientation.REVERSED) {
+      stringResource(Res.string.rune_display_name_reversed, drawnRune.rune.displayName)
+    } else {
+      drawnRune.rune.displayName
+    }
     Text(
       modifier = Modifier.fillMaxWidth(),
-      text = drawnRune.rune.displayName,
+      text = drawnRuneName,
       style = MaterialTheme.typography.headlineSmall,
       textAlign = TextAlign.Center,
     )
+    val imageModifer = Modifier
+      .size(width = 96.dp, height = 144.dp)
+      .padding(top = TakaContentSpacing)
+      .align(Alignment.CenterHorizontally)
+      .then(if (drawnRune.orientation == RuneOrientation.REVERSED) Modifier.rotate(180f) else Modifier)
+    Image(
+      painter = painterResource(drawnRune.rune.drawable()),
+      contentDescription =drawnRuneName,
+      contentScale = ContentScale.Fit,
+      modifier = imageModifer
+    )
     Text(
-      modifier = Modifier.fillMaxWidth(),
+      modifier = Modifier.fillMaxWidth().padding(top = TakaContentSpacing),
       text = stringResource(keywords).toDotSeparatedKeywords(),
       style = MaterialTheme.typography.bodyMedium,
       textAlign = TextAlign.Center,
