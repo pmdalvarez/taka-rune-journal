c81c382e6f65152af6597777ee73e8f30e012ce3
DESKTOP-EGGQ356\Paolo <pmdalvarez@gmail.com>
2026-07-04 13:30:23 +0200

    Structuring view model for new reading screens

--- composeApp/src/commonMain/kotlin/com/taka/runejournal/feature/reading/ui/RuneSpreadResource.kt
+++ composeApp/src/commonMain/kotlin/com/taka/runejournal/feature/reading/ui/RuneSpreadResource.kt
@@ -1,14 +1,10 @@
 package com.taka.runejournal.feature.reading.ui
 
-import com.taka.runejournal.core.domain.model.ReadingTopic
 import com.taka.runejournal.core.domain.model.RuneSpread
 import org.jetbrains.compose.resources.StringResource
 import taka_rune_journal.composeapp.generated.resources.Res
 import taka_rune_journal.composeapp.generated.resources.reading_type_general
-import taka_rune_journal.composeapp.generated.resources.reading_type_purpose
 import taka_rune_journal.composeapp.generated.resources.reading_type_relationships
-import taka_rune_journal.composeapp.generated.resources.reading_type_security
-import taka_rune_journal.composeapp.generated.resources.reading_type_self
 
 fun RuneSpread.name(): StringResource = when (this) {
   RuneSpread.SINGLE_RUNE -> Res.string.reading_type_general
