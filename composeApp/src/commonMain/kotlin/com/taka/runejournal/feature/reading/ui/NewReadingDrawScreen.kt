e7344dc363b1bb168aa2294b88973d08bc9ff44d
DESKTOP-EGGQ356\Paolo <pmdalvarez@gmail.com>
2026-08-07 01:54:02 +0200

    Moved RuneCanvas to its own file and cropped all empty rune images

--- composeApp/src/commonMain/kotlin/com/taka/runejournal/feature/reading/ui/NewReadingDrawScreen.kt
+++ composeApp/src/commonMain/kotlin/com/taka/runejournal/feature/reading/ui/NewReadingDrawScreen.kt
@@ -3,13 +3,9 @@ package com.taka.runejournal.feature.reading.ui
 import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.animation.core.LinearOutSlowInEasing
 import androidx.compose.animation.core.animateFloatAsState
-import androidx.compose.animation.core.animateOffsetAsState
 import androidx.compose.animation.core.tween
 import androidx.compose.animation.fadeOut
-import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.background
-import androidx.compose.foundation.gestures.detectDragGestures
-import androidx.compose.foundation.gestures.detectTapGestures
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.BoxWithConstraints
 import androidx.compose.foundation.layout.Column
@@ -26,27 +22,17 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.key
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
-import androidx.compose.runtime.rememberUpdatedState
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.ImageBitmap
 import androidx.compose.ui.graphics.ImageShader
 import androidx.compose.ui.graphics.ShaderBrush
 import androidx.compose.ui.graphics.TileMode
-import androidx.compose.ui.graphics.drawscope.DrawScope
-import androidx.compose.ui.graphics.drawscope.rotate
-import androidx.compose.ui.graphics.drawscope.scale
-import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.IntOffset
-import androidx.compose.ui.unit.IntSize
 import androidx.compose.ui.unit.dp
 import com.taka.runejournal.core.ui.ImmersiveModeEffect
 import com.taka.runejournal.core.ui.ShakeDetectorEffect
@@ -61,6 +47,7 @@ import com.taka.runejournal.core.ui.components.showErrorSnackbar
 import com.taka.runejournal.core.ui.theme.TakaContentSpacing
 import com.taka.runejournal.core.ui.theme.TakaScreenPadding
 import com.taka.runejournal.core.ui.theme.TakaSectionSpacing
+import com.taka.runejournal.feature.reading.ui.components.RuneCanvas
 import kotlinx.coroutines.delay
 import org.jetbrains.compose.resources.getString
 import org.jetbrains.compose.resources.imageResource
@@ -76,11 +63,6 @@ import taka_rune_journal.composeapp.generated.resources.reading_draw_instruction
 import taka_rune_journal.composeapp.generated.resources.reading_draw_selected_all_runes
 import taka_rune_journal.composeapp.generated.resources.reading_draw_selected_rune_count
 import taka_rune_journal.composeapp.generated.resources.reading_draw_topbar_title
-import taka_rune_journal.composeapp.generated.resources.rune_empty
-import taka_rune_journal.composeapp.generated.resources.rune_empty_brown
-import taka_rune_journal.composeapp.generated.resources.rune_empty_glowing
-import taka_rune_journal.composeapp.generated.resources.rune_empty_half_glowing
-import kotlin.math.roundToInt
 
 @Composable
 fun NewReadingDrawScreen(
@@ -192,7 +174,7 @@ println("XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX onShakingChanged isShaking: $isShaking
       exit = fadeOut(tween(750)) // 1-second fade
     ) {
       val maxOverlayWidth = (maxWidth.value * 0.7).dp
-      IntructionalOverlay(modifier = Modifier.widthIn(max = maxOverlayWidth))
+      InstructionalOverlay(modifier = Modifier.widthIn(max = maxOverlayWidth))
     }
 
     LaunchedEffect(drawState) {
@@ -247,7 +229,7 @@ println("XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX onShakingChanged isShaking: $isShaking
 
 @Preview
 @Composable
-private fun IntructionalOverlay(modifier:Modifier = Modifier.widthIn(max = 500.dp)) {
+private fun InstructionalOverlay(modifier:Modifier = Modifier.widthIn(max = 500.dp)) {
   Column(
     modifier = modifier.fillMaxSize(),
     verticalArrangement = Arrangement.Center,
@@ -279,154 +261,6 @@ private fun IntructionalOverlay(modifier:Modifier = Modifier.widthIn(max = 500.d
   }
 }
 
-@Composable
-private fun RuneCanvas(
-  modifier: Modifier = Modifier,
-  runeCanvasState: RuneCanvasState,
-  drawState: DrawState,
-  onRuneDragStart: (touchPosition: Offset) -> Unit,
-  onRuneDrag: (position: Offset) -> Unit,
-  onRuneDragStop: () -> Unit,
-  onRuneTap: (position: Offset) -> Unit,
-) {
-  val isGestureEnabled = !(drawState == DrawState.Choose.Shaking || drawState is DrawState.Reveal)
-  val emptyRuneImage = imageResource(Res.drawable.rune_empty)
-  val emptyRuneImageGlowing = imageResource(Res.drawable.rune_empty_glowing)
-  val emptyRuneImageHalfGlowing = imageResource(Res.drawable.rune_empty_half_glowing)
-  val emptyRuneImageBrown = imageResource(Res.drawable.rune_empty_brown)
-  val runeImageDrawStates = buildList {
-      runeCanvasState.runeVisualStates
-        .entries
-        .sortedBy { it.value.depth }
-        .forEach { (rune, visualState) ->
-          key(rune) {
-            val animatedCenter by animateOffsetAsState(
-              targetValue = visualState.center,
-              animationSpec = tween(
-                durationMillis = if (drawState is DrawState.Choose)
-                  RuneCanvasState.IMPULSE_INTERVAL_MILLIS.toInt()
-                else
-                  RuneCanvasState.RUNE_REVEAL_ANIMATION_MILLIS.toInt(),
-                easing = LinearOutSlowInEasing
-              ),
-              label = "Rune ${rune.name} Center",
-            )
-            val animatedAngle by animateFloatAsState(
-              targetValue = visualState.angle,
-              animationSpec = tween(
-                durationMillis = if (drawState is DrawState.Choose)
-                  RuneCanvasState.IMPULSE_INTERVAL_MILLIS.toInt()
-                else
-                  RuneCanvasState.RUNE_REVEAL_ANIMATION_MILLIS.toInt(),
-                easing = LinearOutSlowInEasing
-              ),
-              label = "Rune ${rune.name} Angle",
-            )
-            val isDraggedRune = runeCanvasState.draggedRuneState?.rune == rune
-            val isSelected = runeCanvasState.isSelected(rune)
-            val emptyRuneProgress by animateFloatAsState(
-              targetValue = if (drawState is DrawState.Choose && !isSelected) 1f else 0f,
-              animationSpec = tween(durationMillis = RuneCanvasState.RUNE_SELECTION_ANIMATION_MILLIS.toInt(), easing = LinearOutSlowInEasing),
-              label = "Selected Rune ${rune.name} fade in animation",
-            )
-            val glowingEmptyRuneProgress by animateFloatAsState(
-              targetValue = when (drawState) {
-                is DrawState.Choose -> 1 - emptyRuneProgress
-                is DrawState.Reveal.CenteringRunes -> if (isSelected) 1f else 0f
-                is DrawState.Reveal.UnveilingGlyphs -> 0f
-                is DrawState.Reveal.CompletingAnimations -> 0f
-              },
-              animationSpec = tween(durationMillis = RuneCanvasState.RUNE_SELECTION_ANIMATION_MILLIS.toInt(), easing = LinearOutSlowInEasing),
-              label = "Selected Rune ${rune.name} fade in animation",
-            )
-            val glowingGlowingGlyphRuneProgress by animateFloatAsState(
-              targetValue = if (drawState is DrawState.Reveal.UnveilingGlyphs && isSelected) 1f else 0f,
-              animationSpec = tween(durationMillis = RuneCanvasState.RUNE_REVEAL_ANIMATION_MILLIS.toInt(), easing = LinearOutSlowInEasing),
-              label = "Selected Rune ${rune.name} fade in animation",
-            )
-            val glyphRuneProgress by animateFloatAsState(
-              targetValue = if (drawState is DrawState.Reveal.CompletingAnimations && isSelected) 1f else 0f,
-              animationSpec = tween(durationMillis = RuneCanvasState.RUNE_REVEAL_ANIMATION_MILLIS.toInt(), easing = LinearOutSlowInEasing),
-              label = "Selected Rune ${rune.name} fade in animation",
-            )
-            val drawCenter = if (isDraggedRune) visualState.center else animatedCenter
-            val drawAngle = if (isDraggedRune) visualState.angle else animatedAngle
-
-            // Unselected Rune
-            add(RuneImageDrawState(image = emptyRuneImage, center =  drawCenter, angle = drawAngle, alpha = emptyRuneProgress))
-            // Selected Rune - dragged rune cannot be in selected state
-            if (!isDraggedRune) { add(RuneImageDrawState(image = emptyRuneImageGlowing, center =  drawCenter, angle = drawAngle, alpha = glowingEmptyRuneProgress)) }
-            // Rune being unveiled - has glowing glyph
-            add(RuneImageDrawState(image = emptyRuneImageHalfGlowing, center =  drawCenter, angle = drawAngle, alpha = glowingGlowingGlyphRuneProgress))
-            // Rune fully unveiled- has non-glowing glyph
-            add(RuneImageDrawState(image = emptyRuneImageBrown, center =  drawCenter, angle = drawAngle, alpha = glyphRuneProgress))
-          }
-        }
-    }
-  val zoom by animateFloatAsState(
-    targetValue = when {
-      drawState is DrawState.Choose.Shaking || drawState is DrawState.Reveal -> 1.5f
-      else -> 1f
-    },
-    animationSpec = tween(
-      durationMillis = 200,
-      easing = LinearOutSlowInEasing,
-    ),
-    label = "Rune Canvas Zoom",
-  )
-
-  val currentOnRuneDragStart by rememberUpdatedState(onRuneDragStart)
-  val currentOnRuneDrag by rememberUpdatedState(onRuneDrag)
-  val currentOnRuneDragStop by rememberUpdatedState(onRuneDragStop)
-  val currentOnRuneTap by rememberUpdatedState(onRuneTap)
-  Canvas(
-    modifier = modifier.pointerInput(isGestureEnabled) {
-      if (!isGestureEnabled) return@pointerInput
-      detectDragGestures(
-        onDragStart = { position ->
-          currentOnRuneDragStart(position)
-        },
-        onDrag = { change, _ ->
-          change.consume()
-          currentOnRuneDrag(change.position)
-        },
-        onDragEnd = {
-          currentOnRuneDragStop()
-        },
-        onDragCancel = {
-          currentOnRuneDragStop()
-        }
-      )
-    }.pointerInput(isGestureEnabled) {
-      if (!isGestureEnabled) return@pointerInput
-      detectTapGestures(
-        onTap = { position ->
-          currentOnRuneTap(position)
-        }
-      )
-    }
-  ) {
-    scale(
-      scale = zoom,
-      pivot = center,
-    ) {
-      runeImageDrawStates
-        .forEach { runeDrawState ->
-            if (runeDrawState.alpha != 0f) { // skip drawing this if invisible
-              drawRune(
-                runeImage = runeDrawState.image,
-                center = runeDrawState.center,
-                angle = runeDrawState.angle,
-                alpha = runeDrawState.alpha,
-                runeWidth = runeCanvasState.runeWidth,
-                runeHeight = runeCanvasState.runeHeight
-              )
-            }
-        }
-    }
-  }
-}
-
 @Preview
 @Composable
 private fun SelectedRuneCountOverlay(
@@ -454,37 +288,5 @@ private fun SelectedRuneCountOverlay(
     ) {
       Text(stringResource(Res.string.button_reveal_runes))
     }
-  }
-}
-
-private fun DrawScope.drawRune(
-  runeImage: ImageBitmap,
-  center: Offset, // Center point of the rune within the canvas, in pixels.
-  angle: Float, //  Rotation angle in degrees.
-  alpha: Float = 1f, // For crossfading if rune is being selected or selected, represents alpha of unselected image
-  runeWidth: Float,
-  runeHeight: Float,
-) {
-  val topLeft = Offset(
-    x = center.x - runeWidth / 2,
-    y = center.y - runeHeight / 2,
-  )
-
-  rotate(
-    degrees = angle,
-    pivot = center,
-  ) {
-    drawImage(
-      image = runeImage,
-      alpha = alpha,
-      dstOffset = IntOffset(
-        x = topLeft.x.roundToInt(),
-        y = topLeft.y.roundToInt(),
-      ),
-      dstSize = IntSize(
-        width = runeWidth.roundToInt(),
-        height = runeHeight.roundToInt(),
-      ),
-    )
   }
 }
---
+++ composeApp/src/commonMain/kotlin/com/taka/runejournal/feature/reading/ui/components/RuneCanvas.kt
@@ -0,0 +1,214 @@
+package com.taka.runejournal.feature.reading.ui.components
+
+import androidx.compose.animation.core.LinearOutSlowInEasing
+import androidx.compose.animation.core.animateFloatAsState
+import androidx.compose.animation.core.animateOffsetAsState
+import androidx.compose.animation.core.tween
+import androidx.compose.foundation.Canvas
+import androidx.compose.foundation.gestures.detectDragGestures
+import androidx.compose.foundation.gestures.detectTapGestures
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.key
+import androidx.compose.runtime.rememberUpdatedState
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.graphics.ImageBitmap
+import androidx.compose.ui.graphics.drawscope.DrawScope
+import androidx.compose.ui.graphics.drawscope.rotate
+import androidx.compose.ui.graphics.drawscope.scale
+import androidx.compose.ui.input.pointer.pointerInput
+import androidx.compose.ui.unit.IntOffset
+import androidx.compose.ui.unit.IntSize
+import com.taka.runejournal.feature.reading.ui.DrawState
+import com.taka.runejournal.feature.reading.ui.RuneCanvasState
+import com.taka.runejournal.feature.reading.ui.RuneImageDrawState
+import org.jetbrains.compose.resources.imageResource
+import taka_rune_journal.composeapp.generated.resources.Res
+import taka_rune_journal.composeapp.generated.resources.rune_empty
+import taka_rune_journal.composeapp.generated.resources.rune_empty_brown
+import taka_rune_journal.composeapp.generated.resources.rune_empty_glowing
+import taka_rune_journal.composeapp.generated.resources.rune_empty_half_glowing
+import kotlin.collections.component1
+import kotlin.collections.component2
+import kotlin.math.roundToInt
+
+@Composable
+fun RuneCanvas(
+  modifier: Modifier = Modifier,
+  runeCanvasState: RuneCanvasState,
+  drawState: DrawState,
+  onRuneDragStart: (touchPosition: Offset) -> Unit,
+  onRuneDrag: (position: Offset) -> Unit,
+  onRuneDragStop: () -> Unit,
+  onRuneTap: (position: Offset) -> Unit,
+) {
+  val isGestureEnabled = !(drawState == DrawState.Choose.Shaking || drawState is DrawState.Reveal)
+  val emptyRuneImage = imageResource(Res.drawable.rune_empty)
+  val emptyRuneImageGlowing = imageResource(Res.drawable.rune_empty_glowing)
+  val emptyRuneImageHalfGlowing = imageResource(Res.drawable.rune_empty_half_glowing)
+  val emptyRuneImageBrown = imageResource(Res.drawable.rune_empty_brown)
+  val runeImageDrawStates = buildList {
+    runeCanvasState.runeVisualStates
+      .entries
+      .sortedBy { it.value.depth }
+      .forEach { (rune, visualState) ->
+        key(rune) {
+          val animatedCenter by animateOffsetAsState(
+            targetValue = visualState.center,
+            animationSpec = tween(
+              durationMillis = if (drawState is DrawState.Choose)
+                RuneCanvasState.IMPULSE_INTERVAL_MILLIS.toInt()
+              else
+                RuneCanvasState.RUNE_REVEAL_ANIMATION_MILLIS.toInt(),
+              easing = LinearOutSlowInEasing
+            ),
+            label = "Rune ${rune.name} Center",
+          )
+          val animatedAngle by animateFloatAsState(
+            targetValue = visualState.angle,
+            animationSpec = tween(
+              durationMillis = if (drawState is DrawState.Choose)
+                RuneCanvasState.IMPULSE_INTERVAL_MILLIS.toInt()
+              else
+                RuneCanvasState.RUNE_REVEAL_ANIMATION_MILLIS.toInt(),
+              easing = LinearOutSlowInEasing
+            ),
+            label = "Rune ${rune.name} Angle",
+          )
+          val isDraggedRune = runeCanvasState.draggedRuneState?.rune == rune
+          val isSelected = runeCanvasState.isSelected(rune)
+          val emptyRuneProgress by animateFloatAsState(
+            targetValue = if (drawState is DrawState.Choose && !isSelected) 1f else 0f,
+            animationSpec = tween(durationMillis = RuneCanvasState.RUNE_SELECTION_ANIMATION_MILLIS.toInt(), easing = LinearOutSlowInEasing),
+            label = "Selected Rune ${rune.name} fade in animation",
+          )
+          val glowingEmptyRuneProgress by animateFloatAsState(
+            targetValue = when (drawState) {
+              is DrawState.Choose -> 1 - emptyRuneProgress
+              is DrawState.Reveal.CenteringRunes -> if (isSelected) 1f else 0f
+              is DrawState.Reveal.UnveilingGlyphs -> 0f
+              is DrawState.Reveal.CompletingAnimations -> 0f
+            },
+            animationSpec = tween(durationMillis = RuneCanvasState.RUNE_SELECTION_ANIMATION_MILLIS.toInt(), easing = LinearOutSlowInEasing),
+            label = "Selected Rune ${rune.name} fade in animation",
+          )
+          val glowingGlowingGlyphRuneProgress by animateFloatAsState(
+            targetValue = if (drawState is DrawState.Reveal.UnveilingGlyphs && isSelected) 1f else 0f,
+            animationSpec = tween(durationMillis = RuneCanvasState.RUNE_REVEAL_ANIMATION_MILLIS.toInt(), easing = LinearOutSlowInEasing),
+            label = "Selected Rune ${rune.name} fade in animation",
+          )
+          val glyphRuneProgress by animateFloatAsState(
+            targetValue = if (drawState is DrawState.Reveal.CompletingAnimations && isSelected) 1f else 0f,
+            animationSpec = tween(durationMillis = RuneCanvasState.RUNE_REVEAL_ANIMATION_MILLIS.toInt(), easing = LinearOutSlowInEasing),
+            label = "Selected Rune ${rune.name} fade in animation",
+          )
+          val drawCenter = if (isDraggedRune) visualState.center else animatedCenter
+          val drawAngle = if (isDraggedRune) visualState.angle else animatedAngle
+
+          // Unselected Rune
+          add(RuneImageDrawState(image = emptyRuneImage, center =  drawCenter, angle = drawAngle, alpha = emptyRuneProgress))
+          // Selected Rune - dragged rune cannot be in selected state
+          if (!isDraggedRune) { add(RuneImageDrawState(image = emptyRuneImageGlowing, center =  drawCenter, angle = drawAngle, alpha = glowingEmptyRuneProgress)) }
+          // Rune being unveiled - has glowing glyph
+          add(RuneImageDrawState(image = emptyRuneImageHalfGlowing, center =  drawCenter, angle = drawAngle, alpha = glowingGlowingGlyphRuneProgress))
+          // Rune fully unveiled- has non-glowing glyph
+          add(RuneImageDrawState(image = emptyRuneImageBrown, center =  drawCenter, angle = drawAngle, alpha = glyphRuneProgress))
+        }
+      }
+  }
+  val zoom by animateFloatAsState(
+    targetValue = when {
+      drawState is DrawState.Choose.Shaking || drawState is DrawState.Reveal -> 1.5f
+      else -> 1f
+    },
+    animationSpec = tween(
+      durationMillis = 200,
+      easing = LinearOutSlowInEasing,
+    ),
+    label = "Rune Canvas Zoom",
+  )
+
+  val currentOnRuneDragStart by rememberUpdatedState(onRuneDragStart)
+  val currentOnRuneDrag by rememberUpdatedState(onRuneDrag)
+  val currentOnRuneDragStop by rememberUpdatedState(onRuneDragStop)
+  val currentOnRuneTap by rememberUpdatedState(onRuneTap)
+  Canvas(
+    modifier = modifier.pointerInput(isGestureEnabled) {
+      if (!isGestureEnabled) return@pointerInput
+      detectDragGestures(
+        onDragStart = { position ->
+          currentOnRuneDragStart(position)
+        },
+        onDrag = { change, _ ->
+          change.consume()
+          currentOnRuneDrag(change.position)
+        },
+        onDragEnd = {
+          currentOnRuneDragStop()
+        },
+        onDragCancel = {
+          currentOnRuneDragStop()
+        }
+      )
+    }.pointerInput(isGestureEnabled) {
+      if (!isGestureEnabled) return@pointerInput
+      detectTapGestures(
+        onTap = { position ->
+          currentOnRuneTap(position)
+        }
+      )
+    }
+  ) {
+    scale(
+      scale = zoom,
+      pivot = center,
+    ) {
+      runeImageDrawStates
+        .forEach { runeDrawState ->
+          if (runeDrawState.alpha != 0f) { // skip drawing this if invisible
+            drawRune(
+              runeImage = runeDrawState.image,
+              center = runeDrawState.center,
+              angle = runeDrawState.angle,
+              alpha = runeDrawState.alpha,
+              runeWidth = runeCanvasState.runeWidth,
+              runeHeight = runeCanvasState.runeHeight
+            )
+          }
+        }
+    }
+  }
+}
+
+private fun DrawScope.drawRune(
+  runeImage: ImageBitmap,
+  center: Offset, // Center point of the rune within the canvas, in pixels.
+  angle: Float, //  Rotation angle in degrees.
+  alpha: Float = 1f, // For crossfading if rune is being selected or selected, represents alpha of unselected image
+  runeWidth: Float,
+  runeHeight: Float,
+) {
+  val topLeft = Offset(
+    x = center.x - runeWidth / 2,
+    y = center.y - runeHeight / 2,
+  )
+
+  rotate(
+    degrees = angle,
+    pivot = center,
+  ) {
+    drawImage(
+      image = runeImage,
+      alpha = alpha,
+      dstOffset = IntOffset(
+        x = topLeft.x.roundToInt(),
+        y = topLeft.y.roundToInt(),
+      ),
+      dstSize = IntSize(
+        width = runeWidth.roundToInt(),
+        height = runeHeight.roundToInt(),
+      ),
+    )
+  }
+}
