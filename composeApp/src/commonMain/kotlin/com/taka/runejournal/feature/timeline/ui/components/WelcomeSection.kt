31e78b76f61e42a4302e378102bdec052af4ae9d
DESKTOP-EGGQ356\Paolo <pmdalvarez@gmail.com>
2026-09-06 23:11:23 +0200

    Changing welcome section to use pager / slides

--- composeApp/src/commonMain/kotlin/com/taka/runejournal/feature/timeline/ui/components/WelcomeSection.kt
+++ composeApp/src/commonMain/kotlin/com/taka/runejournal/feature/timeline/ui/components/WelcomeSection.kt
@@ -4,7 +4,6 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
@@ -13,7 +12,6 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import com.taka.runejournal.core.ui.components.TakaButton
-import com.taka.runejournal.core.ui.theme.TakaContentSpacing
 import com.taka.runejournal.core.ui.theme.TakaIconButtonSize
 import com.taka.runejournal.core.ui.theme.TakaSectionSpacing
 import com.taka.runejournal.core.ui.theme.TakaSpaceSm
